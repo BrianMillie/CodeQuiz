b4faa2cd27b518f4f61f0ae96b276bb5e15ff8cc
BrianMillie <daniel.shipp@uk.verizon.com>
2022-12-06 08:14:53 +0000

    Added some functions and dynamic elements

---
+++ assets/js/questions.js
@@ -0,0 +1,22 @@
+var queries = [{
+    "query": 'Determine the result: String(“Pizza”) === “Pizza”;',
+    "options": ["True", "False", "Undefined", "Null"],
+    "answer": "True",
+},
+{
+    "query": 'How would you round 275.53 to the nearest whole number',
+    "options": ["round(275.53)", "round.math(275.53)", "Math.round(275.53)", "Math.rnd(275.53)"],
+    "answer": "Math.round(275.53)",
+},
+{
+    "query": 'Where should the java script tag be placed in the html',
+    "options": ["After the Footer", "At the very top of the file", "In the Header section", "At the Bottom of the Body"],
+    "answer": "At the Bottom of the Body",
+},
+{
+    "query": 'What does a prompt function do?',
+    "options": ["Brings up a message and user input box", "Shows information only in a message", "Tells you the time in a message", "None of the above"],
+    "answer": "Brings up a message and user input box",
+},
+
+]
